ca7b322662d8f209ebfb28498db14406e041ade4
Sanzhar <dos.0227@yandex.ru>
2023-01-04 19:56:21 +0600

    Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 11);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("{ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],3}\t");
+        }
+        Console.Write("}");
+        Console.WriteLine();
+    }
+}
+
+int[,] SortArray(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int temp = array[0, j];
+        array[0, j] = array[array.GetLength(0) - 1, j];
+        array[array.GetLength(0) - 1, j] = temp;
+    }
+    return array;
+}
+
+Console.WriteLine("Введите количество строк двумерного массива:");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+Console.WriteLine($"Сгенерирован массив из {array.GetLength(0)} строк и {array.GetLength(1)} столбцов:");
+PrintArray(array);
+Console.WriteLine($"Первая и последняя строки массива были заменены местами, измененный массив:");
+PrintArray(SortArray(array));
